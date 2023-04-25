c1b175907223d8600c9b6c600ccd67e7d04bffc1
Amro Al-Wageei <shandaqamro@gmail.com>
2023-04-25 15:54:11 +0300

    Cars-section

--- JS/swiperjs.js
+++ JS/swiperjs.js
@@ -31,13 +31,36 @@
 const swiperr = new Swiper(".swiper", {
   speed: 1000,
   spaceBetween: -50,
-  slidesPerView: 4,
+  slidesPerView: 5,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 0,
+    },
+    522: {
+      slidesPerView: 1,
+    },
+    767: {
+      slidesPerView: 2,
+    },
+
+    1050: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+    },
   },
 });
